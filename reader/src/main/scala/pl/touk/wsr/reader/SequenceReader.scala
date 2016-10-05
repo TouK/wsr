aecccba3148365610bbeed32e7a3a03b73e6292d
Maxymilian Śmiech <msc@touk.pl>
2016-10-05 11:16:46 +0200

    added SequenceReader & test

---
+++ reader/src/main/scala/pl/touk/wsr/reader/SequenceReader.scala
@@ -0,0 +1,56 @@
+package pl.touk.wsr.reader
+
+import java.util.UUID
+
+import akka.actor.{Actor, Props}
+import com.typesafe.scalalogging.StrictLogging
+import pl.touk.wsr.protocol.srvrdr.{Ack, EndOfSequence, NextNumberInSequence, RequestForSequence}
+import pl.touk.wsr.transport.WsrClient
+
+object SequenceReader {
+
+  def props(seqId: UUID,
+            client: WsrClient): Props = {
+    Props(new SequenceReader(
+      seqId,
+      client))
+  }
+
+}
+
+private class SequenceReader(seqId: UUID,
+                             client: WsrClient)
+  extends Actor
+    with StrictLogging {
+
+  import context._
+
+  logger.debug(s"Sequence $seqId: start")
+
+  client.send(RequestForSequence(seqId))
+
+  def receive = receiveCommon orElse {
+    case NextNumberInSequence(_, number) =>
+      logger.debug(s"Sequence $seqId: received value $number")
+      client.send(Ack(seqId))
+      become(receiveNext(number))
+  }
+
+  def receiveNext(previous: Int): Receive = receiveCommon orElse {
+    case NextNumberInSequence(_, number) =>
+      logger.debug(s"Sequence $seqId: received value $number")
+      if (number != previous + 1) {
+        logger.error(s"Sequence $seqId: out-of-order value $number after value $previous")
+      }
+      client.send(Ack(seqId))
+      become(receiveNext(number))
+  }
+
+  def receiveCommon: Receive = {
+    case EndOfSequence(_) =>
+      logger.debug(s"Sequence $seqId: received end of sequence")
+      client.send(Ack(seqId))
+      stop(self)
+  }
+
+}
---
+++ reader/src/test/scala/pl/touk/wsr/reader/SequenceReaderSpec.scala
@@ -0,0 +1,80 @@
+package pl.touk.wsr.reader
+
+import java.util.UUID
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.testkit.TestKit
+import org.scalatest.{FlatSpecLike, Matchers}
+import pl.touk.wsr.protocol.ClientMessage
+import pl.touk.wsr.protocol.srvrdr.{Ack, EndOfSequence, NextNumberInSequence, RequestForSequence}
+import pl.touk.wsr.transport.WsrClient
+
+class SequenceReaderSpec
+  extends TestKit(ActorSystem("SequenceReaderSpec"))
+    with FlatSpecLike
+    with Matchers {
+
+  it should "send request for sequence" in {
+    prepareSequenceReader()
+    expectMsgType[RequestForSequence]
+  }
+
+  it should "process first number" in {
+    val reader = prepareSequenceReader()
+    expectMsgType[RequestForSequence]
+    reader ! NextNumberInSequence(null, 1)
+    expectMsgType[Ack]
+  }
+
+  it should "process second number" in {
+    val reader = prepareSequenceReader()
+    expectMsgType[RequestForSequence]
+    reader ! NextNumberInSequence(null, 1)
+    expectMsgType[Ack]
+    reader ! NextNumberInSequence(null, 2)
+    expectMsgType[Ack]
+  }
+
+  it should "process out-of-order number" in {
+    val reader = prepareSequenceReader()
+    expectMsgType[RequestForSequence]
+    reader ! NextNumberInSequence(null, 1)
+    expectMsgType[Ack]
+    reader ! NextNumberInSequence(null, 3)
+    expectMsgType[Ack]
+  }
+
+  it should "handle end of empty sequence" in {
+    val reader = prepareSequenceReader()
+    expectMsgType[RequestForSequence]
+    reader ! EndOfSequence(null)
+    expectMsgType[Ack]
+    expectTerminated(watch(reader))
+  }
+
+  it should "handle end of sequence" in {
+    val reader = prepareSequenceReader()
+    expectMsgType[RequestForSequence]
+    reader ! NextNumberInSequence(null, 1)
+    expectMsgType[Ack]
+    reader ! NextNumberInSequence(null, 2)
+    expectMsgType[Ack]
+    reader ! EndOfSequence(null)
+    expectMsgType[Ack]
+    expectTerminated(watch(reader))
+  }
+
+  def prepareSequenceReader(): ActorRef = {
+    val seqId = UUID.randomUUID()
+    system.actorOf(
+      SequenceReader.props(
+        seqId,
+        new WsrClient {
+          def send(message: ClientMessage): Unit = {
+            testActor ! message
+          }
+        }),
+      seqId.toString)
+  }
+
+}
