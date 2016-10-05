ca20313f731df13a7697dd8236de3fe1764e2701
Maxymilian Śmiech <msc@touk.pl>
2016-10-05 14:40:15 +0200

    better connection handling in Writer

--- writer/src/main/scala/pl/touk/wsr/writer/Writer.scala
+++ writer/src/main/scala/pl/touk/wsr/writer/Writer.scala
@@ -1,29 +1,63 @@
 package pl.touk.wsr.writer
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, Props, Stash, Status}
+import akka.pattern.pipe
 import com.typesafe.scalalogging.LazyLogging
+import pl.touk.wsr.protocol.ServerMessage
 import pl.touk.wsr.protocol.wrtsrv.{NextNumber, RequestForNumbers}
-import pl.touk.wsr.transport.WsrClientSender
+import pl.touk.wsr.transport.{WsrClientFactory, WsrClientHandler, WsrClientSender}
 
 object Writer {
 
-  def props(client: WsrClientSender): Props = {
-    Props(new Writer(client))
+  def props(clientFactory: WsrClientFactory): Props = {
+    Props(new Writer(clientFactory))
   }
+
+  case object ConnectionLost
 
 }
 
-class Writer(client: WsrClientSender)
+class Writer(clientFactory: WsrClientFactory)
   extends Actor
+    with Stash
     with LazyLogging {
 
+  import Writer._
+  import context._
+
+  logger.debug("Start")
+
+  clientFactory.connect(new WsrClientHandler {
+    def onMessage(message: ServerMessage): Unit = {
+      self ! message
+    }
+
+    def onConnectionLost(): Unit = {
+      self ! ConnectionLost
+    }
+  }) pipeTo self
+
   def receive = {
+    case client: WsrClientSender =>
+      logger.debug("Connected")
+      unstashAll()
+      become(receiveConnected(client))
+    case Status.Failure(cause) =>
+      logger.error("Exception while connecting", cause)
+      system.terminate()
+    case _ =>
+      stash()
+  }
+
+  def receiveConnected(client: WsrClientSender): Receive = {
     case RequestForNumbers(start, count) =>
       logger.debug(s"Received request for $count numbers starting from $start")
       start until (start + count) foreach {
         number =>
           client.send(NextNumber(number))
       }
+    case ConnectionLost =>
+      logger.error("Connection lost")
   }
 
 }
--- writer/src/test/scala/pl/touk/wsr/writer/WriterSpec.scala
+++ writer/src/test/scala/pl/touk/wsr/writer/WriterSpec.scala
@@ -1,11 +1,16 @@
 package pl.touk.wsr.writer
 
-import akka.actor.{ActorRef, ActorSystem}
+import java.util.concurrent.TimeUnit
+
+import akka.actor.ActorSystem
 import akka.testkit.TestKit
 import org.scalatest.{FlatSpecLike, Matchers}
 import pl.touk.wsr.protocol.ClientMessage
 import pl.touk.wsr.protocol.wrtsrv.{NextNumber, RequestForNumbers}
-import pl.touk.wsr.transport.WsrClientSender
+import pl.touk.wsr.transport.{WsrClientFactory, WsrClientHandler, WsrClientSender}
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future, Promise}
 
 class WriterSpec
   extends TestKit(ActorSystem("WriterSpec"))
@@ -13,33 +18,42 @@ class WriterSpec
     with Matchers {
 
   it should "generate numbers" in {
-    val writer = prepareWriter()
-    writer ! RequestForNumbers(1, 2)
+    val handler = prepareWriter()
+    handler.onMessage(RequestForNumbers(1, 2))
     expectMsg(NextNumber(1))
     expectMsg(NextNumber(2))
     expectNoMsg()
   }
 
   it should "handle many requests for numbers" in {
-    val writer = prepareWriter()
-    writer ! RequestForNumbers(1, 2)
+    val handler = prepareWriter()
+    handler.onMessage(RequestForNumbers(1, 2))
     expectMsg(NextNumber(1))
     expectMsg(NextNumber(2))
-    writer ! RequestForNumbers(5, 3)
+    handler.onMessage(RequestForNumbers(5, 3))
     expectMsg(NextNumber(5))
     expectMsg(NextNumber(6))
     expectMsg(NextNumber(7))
     expectNoMsg()
   }
 
-  def prepareWriter(): ActorRef = {
+  def prepareWriter(): WsrClientHandler = {
+    val promise = Promise[WsrClientHandler]()
     system.actorOf(
       Writer.props(
-        new WsrClientSender {
-          def send(message: ClientMessage): Unit = {
-            testActor ! message
+        new WsrClientFactory {
+          def connect(handler: WsrClientHandler): Future[WsrClientSender] = {
+            promise.success(handler)
+            Future.successful {
+              new WsrClientSender {
+                def send(message: ClientMessage): Unit = {
+                  testActor ! message
+                }
+              }
+            }
           }
         }))
+    Await.result(promise.future, Duration(10, TimeUnit.SECONDS))
   }
 
 }
